04dbc568b8ced58071788bd9ad083c7afe317167
pkuwwt <wwthunan@gmail.com>
2020-03-16 22:23:15 +0800

    update menu on mobiles

--- js/utils.js
+++ js/utils.js
@@ -1,7 +1,12 @@
 
+const is_visible = id => {
+    const e = (typeof id === 'string') ? document.getElementById(id) : id;
+    return e && e.offsetParent !== null;
+};
+
 const show_element = id => {
     const e = (typeof id === 'string') ? document.getElementById(id) : id;
-    e.style.display = '';
+    e.style.display = 'block';
 };
 
 const hide_element = id => {
@@ -11,19 +16,19 @@ const hide_element = id => {
 
 const toggle_element = id => {
     const e = (typeof id === 'string') ? document.getElementById(id) : id;
-    if (e.style.display == '') {
+    if (is_visible(e)) {
         e.style.display = 'none';
     } else {
-        e.style.display = '';
+        e.style.display = 'block';
     }
 };
 
 const toggle_child_element = (el, id) => {
     const e = el.querySelector('#' + id);
-    if (e.style.display == '') {
+    if (e.style.display !== 'none') {
         e.style.display = 'none';
     } else {
-        e.style.display = '';
+        e.style.display = 'block';
     }
 };
 
